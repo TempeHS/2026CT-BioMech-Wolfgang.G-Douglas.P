cf49346418ac1c7571e30168225407178bd39502
Wolfgang G <wolfgang.gledhill@education.nsw.gov.au>
2025-12-01 12:43:13 +1100

    The Heart Rate sensor stuff aint workin

--- VHR-Monitor/HeartRateClip-Wolfgang/HeartRateClip-Wolfgang.ino
+++ VHR-Monitor/HeartRateClip-Wolfgang/HeartRateClip-Wolfgang.ino
@@ -1,9 +1,30 @@
-void  HeartRateClip_setup() {
-  
+#include <DFRobot_Heartrate.h>
 
+const int HR_PIN = 6;
+DFRobot_Heartrate heartRate(HR_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("=== Heart Rate Clip Sensor ===");
+  Serial.println("Sensor initialized on pin D6");
+  Serial.println("Position ear clip on earlobe");
+  delay(2000);
+}
+
+void loop() {
+  HeartRateClip();
+}
+
+void HeartRateClip_setup() {
+  Serial.println("Setup complete - ready to measure");
 }
 
 void HeartRateClip() {
-
-
+  int BPM = heartRate.getRate();  // Fixed: Changed getHeartRate() to getRate()
+  
+  Serial.print("Heart Rate: ");
+  Serial.print(BPM);
+  Serial.println(" BPM");
+  
+  delay(500);
 }
--- VHR-Monitor/HeartRateClip-Wolfgang/WolfgangNotes.md
+++ VHR-Monitor/HeartRateClip-Wolfgang/WolfgangNotes.md
@@ -1,1 +1,4 @@
 WOLFGANG DO NOT FUCK WITH THE FILES IF YOU CAN AVOID IT, IT DID WEIRD VOODO SHIT IN THE FIRST LESSON WHEN YOU DID THAT, DO NOT DO IT AGAIN!!!!!
+
+1/12/2025 I'm struggling with the heart rate sensor, it keeps running 0BPM on the serial monitor
+Had to download PulseSensor Playground
