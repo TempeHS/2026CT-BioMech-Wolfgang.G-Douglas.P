6e9bcb936aa7d475c534ed7dfec319abcfe05b39
Wolfgang G <wolfgang.gledhill@education.nsw.gov.au>
2025-12-10 09:37:05 +1100

    I'm tired

--- VHR-Monitor/HeartRateClip-Wolfgang/HeartRateClipWolfgang/HeartRateClipWolfgang.ino
+++ VHR-Monitor/HeartRateClip-Wolfgang/HeartRateClipWolfgang/HeartRateClipWolfgang.ino
@@ -1,23 +1,14 @@
- // Function: This program can be used to measure heart rate, the lowest pulse in the program be set to 30.
-    //         Use an external interrupt to measure it.
-    // Hardware: Grove - Ear-clip Heart Rate Sensor, Grove - Base Shield, Grove - LED
-    // Arduino IDE: Arduino-1.0
-    // Author: FrankieChu       
-    // Date: Jan 22, 2013
-    // Version: v1.0
-    // by www.seeedstudio.com
     #define LED 4//indicator, Grove - LED is connected with D4 of Arduino
     boolean led_state = LOW;//state of LED, each time an external interrupt 
                                     //will change the state of LED
-    unsigned char counter;
-    unsigned long temp[21];
-    unsigned long sub;
-    bool data_effect=true;
-    unsigned int heart_rate;//the measurement result of heart rate
+        unsigned char counter;
+        unsigned long temp[21];
+        unsigned long sub;
+        bool data_effect=true;
+        unsigned int heart_rate;//the measurement result of heart rate
 
-    const int max_heartpluse_duty = 2000;//you can change it follow your system's request.
-                            //2000 meams 2 seconds. System return error 
-                            //if the duty overtrip 2 second.
+    const int max_heartpluse_duty = 2000;
+                        
     void setup()
     {
         pinMode(LED, OUTPUT);
@@ -26,24 +17,24 @@
         delay(5000);
         arrayInit();
         Serial.println("Heart rate test begin.");
-        attachInterrupt(0, interrupt, RISING);//set interrupt 0,digital port 2
+        attachInterrupt(0, interrupt, RISING);
     }
     void loop()
     {
-        digitalWrite(LED, led_state);//Update the state of the indicator
+        digitalWrite(LED, led_state);
     }
-    /*Function: calculate the heart rate*/
+
     void sum()
     {
      if(data_effect)
         {
-          heart_rate=1200000/(temp[20]-temp[0]);//60*20*1000/20_total_time 
+          heart_rate=1200000/(temp[20]-temp[0]);
           Serial.print("Heart_rate_is:\t");
           Serial.println(heart_rate);
         }
-       data_effect=1;//sign bit
+       data_effect=1;
     }
-    /*Function: Interrupt service routine.Get the sigal from the external interrupt*/
+
     void interrupt()
     {
         temp[counter]=millis();
