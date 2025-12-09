5f8ffd6787cad7fa40ee1c448ff4644279d0310c
Douglas P <douglas.percival@education.nsw.gov.au>
2025-12-09 22:29:33 +0000

    blahblahblah

--- VHR-Monitor/douglas/Header.h
+++ VHR-Monitor/douglas/Header.h
@@ -3,9 +3,29 @@
 
 #include <Arduino.h>
 
-void ledControllerBegin(uint8_t pin);
-void ledControllerActivate();
-void ledControllerDeactivate();
-void ledControllerLoop();
+void ButtondouglasBegin(uint8_t pin);
+void ButtondouglasActivate();
+void ButtondouglasDeactivate();
+void ButtondouglasLoop();
+
+void LEDdpigasBegin(uint8_t pin);
+void LEDdpigasActivate();
+void LEDdpigasDeactivate();
+void LEDdpigasLoop();
+
+void OLEDdouglasBegin(uint8_t pin);
+void OLEDdouglasActivate();
+void OLEDdouglasDeactivate();
+void OLEDdouglasLoop();
+
+void BuzzerWolfgangBegin(uint8_t pin);
+void BuzzerWolfgangActivate();
+void BuzzerWolfgangDeactivate();
+void BuzzerWolfgangLoop();
+
+void HeartRateClipWolfgangBegin(uint8_t pin);
+void HeartRateClipWolfgangActivate();
+void HeartRateClipWolfgangDeactivate();
+void HeartRateClipWolfgangLoop();
 
 #endif
